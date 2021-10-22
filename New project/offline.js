edf8cb8d65fd1c2be30970890ef2ab369541c00d
Slavlned <91011801+Slavlned@users.noreply.github.com>
2021-10-22 22:36:28 +0500

    Add files via upload

---
+++ New project/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1634923949,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/unnamed-sheet0.png",
+		"images/1450705189160574191removebgpreview-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
